f9ca3adf48702c5e532fdb4f33c224b6baf627a2
INTI-0213 <INTI-0213@INTI0213>
2016-06-15 17:38:08 +0200

    modele biens et proprio : associations (a faire avec class concrete)

--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/modele/BienImmobilier.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/modele/BienImmobilier.java
@@ -2,25 +2,17 @@ package com.adaming.appSystemeAgence.modele;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Table;
 
 @MappedSuperclass
 public abstract class BienImmobilier implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name = "id_b_immo")
-//	private int id;
 	
 //	@Column(name = "code_standard")
 //	private int codeStandard;
@@ -45,6 +37,12 @@ public abstract class BienImmobilier implements Serializable {
 	@Column(name = "num_contrat")
 	private String numContrat = null;
 
+	/////////// ASSOCIATIONS ///////////
+	
+	@ManyToOne (fetch = FetchType.EAGER)
+    @JoinColumn (name = "proprietaire_id", 
+                 referencedColumnName = "id_proprietaire")
+    private Proprietaire proprietaire;
 	
 	/* Constructeurs. */
 	public BienImmobilier() {
--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/modele/Proprietaire.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/modele/Proprietaire.java
@@ -1,18 +1,21 @@
 package com.adaming.appSystemeAgence.modele;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity(name = "proprietaire")
 @Table(name = "proprietaires")
 public class Proprietaire extends Personne implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +27,10 @@ public class Proprietaire extends Personne implements Serializable {
 	
 //	private List<BienImmobilier> listeBiens;
 
+	/////////// ASSOCIATIONS ///////////
+	
+	@OneToMany(mappedBy = "proprietaire")
+    private Collection<BienImmobilier> listeBiens;
 	
 	/* Constructeurs. */
 	public Proprietaire() {
