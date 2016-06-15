cc4239c5cbbd74aba9ad82b56fafda788b6fb9a6
Philippe <Philippe@192.168.102.53>
2016-06-15 09:54:02 +0200

    Mise à jour de la méthode initConseiller du haricot géré (^^) par Phifouille.

--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
@@ -17,25 +17,26 @@ public class AgenceBean implements Serializable {
 private static final long serialVersionUID = 1L;
 	
 	private List<Conseiller> conseillerList;
+	private Conseiller conseiller;
 
-	
 	//la couche service : injection du service dans le managedbean
 	@ManagedProperty(value="#{conseillerServiceBean}")
 	private IConseillerService conseillerService;
 
 	/**
-	 * ctor 
+	 * Constructeur. 
 	 */
 	public AgenceBean() {
 		super();
 	}
 	
 	/**
-	* Récupération de la liste des conseillers.
-	*/
+	 * Récupération de la liste des conseillers.
+	 * @return
+	 */
 	public List<Conseiller> getAllConseillers() {
 		System.out.println("===== Entrée dans la méthode getAllConseillers de AgenceBean.java. =====");
-		conseillerList = conseillerService.getAllConseillers();
+		conseillerService.getAllConseillers();
 		System.out.println("===== Fin de la méthode getAllConseillers de AgenceBean.java. =====");
 		return conseillerList;
 	}
@@ -43,18 +44,36 @@ private static final long serialVersionUID = 1L;
 	/**
 	* Initialisation d'un conseiller.
 	*/
-	public void initConseiller(Conseiller pConseiller) {
+	public void initConseiller() {
+		// Méthode pompée du projet 10_gestionBibliotheque_DAO.
+		
 		System.out.println("===== Entrée dans la méthode initConseiller de AgenceBean.java. =====");
-		conseillerService.initConseiller(pConseiller);
+		setConseiller(new Conseiller());
 		System.out.println("===== Fin de la méthode initConseiller de AgenceBean.java. =====");
 	}
 
 	/**
-	* Ajout d'un conseiller.
-	*/
+	 * Ajout d'un conseiller.
+	 * @param pConseiller
+	 */
 	public void addConseiller(Conseiller pConseiller) {
 		System.out.println("===== Entrée dans la méthode addConseiller de AgenceBean.java. =====");
 		conseillerService.addConseiller(pConseiller);
 		System.out.println("===== Fin de la méthode addConseiller de AgenceBean.java. =====");
+	}
+	
+	
+	/* Acceseurs et mutateurs. */
+	public List<Conseiller> getConseillerList() {
+		return conseillerList;
+	}
+	public void setConseillerList(List<Conseiller> conseillerList) {
+		this.conseillerList = conseillerList;
+	}
+	public Conseiller getConseiller() {
+		return conseiller;
+	}
+	public void setConseiller(Conseiller conseiller) {
+		this.conseiller = conseiller;
 	}
 }
