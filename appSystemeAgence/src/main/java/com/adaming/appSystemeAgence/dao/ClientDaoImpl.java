2a2538b2a73824cf67713daf847ec77b8f45196b
INTI-0213 <INTI-0213@INTI0213>
2016-06-20 14:00:17 +0200

    DAO Client : @repository

--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/dao/ClientDaoImpl.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/dao/ClientDaoImpl.java
@@ -6,31 +6,34 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.adaming.appSystemeAgence.modele.Client;
 
+@Repository
 public class ClientDaoImpl implements IClientDao {
 
 	@Autowired
 	private SessionFactory sf;	// Récupération de la fabrique de session.
 	
 	public List<Client> getAllClients() {
-		System.out.println("===== ClientDaoImpl.java : entrée dans la méthode getAllClients. =====");
+		System.out.println("=====> DAO : getAllClients");
 		
 		Session session = sf.openSession();		// Ouverture de la session.
 		String hqlReq = "FROM client";
 		Query requete = session.createQuery(hqlReq);
-			@SuppressWarnings("unchecked")
+		
+		@SuppressWarnings("unchecked")
 		List<Client> listeClients = requete.list();
 		
-		System.out.println("===== ClientDaoImpl.java : listeClients récupérée" + listeClients + ". =====");
-		System.out.println("===== ClientDaoImpl.java : sortie de la méthode getAllClients. =====");
+		System.out.println("=====> DAO : listeClients : " + listeClients);
 		
 		return listeClients;
 	}
 
 	public boolean addClient(Client pClient) {
-		// TODO Auto-generated method stub
+		System.out.println("=====> DAO : add Client : " + pClient);
+		System.out.println("=====> DAO : add Client : methode a faire.");
 		return false;
 	}
 
