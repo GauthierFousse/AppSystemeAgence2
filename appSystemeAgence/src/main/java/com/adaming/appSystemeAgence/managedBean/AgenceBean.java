8ff91d6542bfff1c8f6909cec61b0c158c16a929
Philippe <Philippe@192.168.1.15>
2016-06-21 23:18:29 +0200

    Ajout de trucs pour le bouton éditer du tableau de bord (page editionConseiller.xhtml inutile ?) par Phifouille.

--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
@@ -8,6 +8,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIParameter;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -28,7 +29,7 @@ import com.adaming.appSystemeAgence.service.IClientService;
 import com.adaming.appSystemeAgence.service.IConseillerService;
 import com.adaming.appSystemeAgence.service.IProprietaireService;
 
-@ManagedBean(name="agenceMB")
+@ManagedBean(name = "agenceMB")
 @SessionScoped
 public class AgenceBean implements Serializable {
 
@@ -56,23 +57,23 @@ public class AgenceBean implements Serializable {
 	/*
 	 * Injection des services dans le managedbean.
 	 */
-	@ManagedProperty(value="#{conseillerServiceBean}")
+	@ManagedProperty(value = "#{conseillerServiceBean}")
 	private IConseillerService conseillerService;
 
-	@ManagedProperty(value="#{proprietaireServiceBean}")
+	@ManagedProperty(value = "#{proprietaireServiceBean}")
 	private IProprietaireService proprietaireService;
 
-	@ManagedProperty(value="#{clientServiceBean}")
+	@ManagedProperty(value = "#{clientServiceBean}")
 	private IClientService clientService;
 
-	@ManagedProperty(value="#{bienServiceBean}")
+	@ManagedProperty(value = "#{bienServiceBean}")
 	private IBienService bienService;
 
-	@ManagedProperty(value="#{classeStandardServiceBean}")
+	@ManagedProperty(value = "#{classeStandardServiceBean}")
 	private IClasseStandardService classeStandardService;
 
 	/**
-	 * Constructeur. 
+	 * Constructeur.
 	 */
 	public AgenceBean() {
 		super();
@@ -92,8 +93,8 @@ public class AgenceBean implements Serializable {
 		bienAAcheter.setAdresse(new Adresse());
 
 		classeStandard = new ClasseStandard();
-		classeStandard.setTypeBien(" ");	
-		}
+		classeStandard.setTypeBien(" ");
+	}
 
 	/**
 	 * Instanciation d'un nouveau conseiller.
@@ -104,6 +105,7 @@ public class AgenceBean implements Serializable {
 		conseiller.setAdresse(new Adresse());
 		System.out.println("===> MB : new conseiller set.");
 	}
+
 	/**
 	 * Instanciation d'un nouveau Proprietaire.
 	 */
@@ -113,6 +115,7 @@ public class AgenceBean implements Serializable {
 		proprietaire.setAdresse(new Adresse());
 		System.out.println("===> MB : new Proprietaire set.");
 	}
+
 	/**
 	 * Instanciation d'un nouveau Bien a louer.
 	 */
@@ -122,6 +125,7 @@ public class AgenceBean implements Serializable {
 		bienALouer.setAdresse(new Adresse());
 		System.out.println("===> MB : new Bien a louer set.");
 	}
+
 	/**
 	 * Instanciation d'un nouveau Bien a acheter.
 	 */
@@ -147,18 +151,19 @@ public class AgenceBean implements Serializable {
 	 */
 	public void addConseiller() {
 		System.out.println("===> MB : add conseiller : " + conseiller);
-		if (getConseillerService().addConseiller(conseiller)){
+		if (getConseillerService().addConseiller(conseiller)) {
 			System.out.println("===> MB : conseiller added with success");
 		} else {
 			System.out.println("===> MB : conseiller FAILED to add.");
 		}
 	}
+
 	/**
 	 * Ajout d'un proprietaire dans la BDD.
 	 */
 	public void addProprietaire() {
 		System.out.println("===> MB : add Proprietaire : " + proprietaire);
-		if (getProprietaireService().addProprietaire(proprietaire)){
+		if (getProprietaireService().addProprietaire(proprietaire)) {
 			System.out.println("===> MB : Proprietaire added with success");
 		} else {
 			System.out.println("===> MB : Proprietaire FAILED to add.");
@@ -173,61 +178,72 @@ public class AgenceBean implements Serializable {
 		FacesMessage message = null;
 		boolean validForm = false;
 
-		if(true) {
+		if (true) {
 			validForm = true;
-			System.out.println("===> MB : add classeStandard : " + classeStandard);
-			if (getClasseStandardService().addClasseStandard(classeStandard)){
-				System.out.println("===> MB : classeStandard added with success to DB");
-				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "SuccÃ¨s" ,"classeStandard added with success to DB");
+			System.out.println("===> MB : add classeStandard : "
+					+ classeStandard);
+			if (getClasseStandardService().addClasseStandard(classeStandard)) {
+				System.out
+						.println("===> MB : classeStandard added with success to DB");
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"SuccÃ¨s", "classeStandard added with success to DB");
 			} else {
-				System.out.println("===> MB : classeStandard FAILED to add to DB");
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur", "classeStandard FAILED to add to DB");
+				System.out
+						.println("===> MB : classeStandard FAILED to add to DB");
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN,
+						"Erreur", "classeStandard FAILED to add to DB");
 			}
 
 		} else {
 			validForm = false;
-			message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur", "Veuillez vÃ©rifier les champs");
+			message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur",
+					"Veuillez vÃ©rifier les champs");
 		}
 
 		FacesContext.getCurrentInstance().addMessage(null, message);
 		context.addCallbackParam("validForm", validForm);
-	}   
+	}
 
 	/**
 	 * Suppression d'une classe standard dans la BDD.
 	 */
 	public void removeClasseStandard(ActionEvent e) {
 		System.out.println("!!!!!!!!!!! removeClasseStandard !!!!!!!!!!!!!!!");
-		//recup du parametre id de <f:param>
-		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		// recup du parametre id de <f:param>
+		Map<String, String> params = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap();
 		int idClasseStandard = Integer.parseInt(params.get("classeStdId"));
 		System.out.println("===> MB : PARAM ID : " + idClasseStandard);
 
-		//on va supprimer la classeStandard de la Bdd
-		
+		// on va supprimer la classeStandard de la Bdd
+
 		classeStandardService.deleteClasseStandardById(idClasseStandard);
-		System.out.println("===> MB : PROPRIETAIRE SUPPRIME : "+ idClasseStandard);
+		System.out.println("===> MB : PROPRIETAIRE SUPPRIME : "
+				+ idClasseStandard);
 
-	}   
-
+	}
 
 	/**
 	 * 
 	 */
-	public void selectProprietaire(){
-		//recup du parametre id de <f:param>
-		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+	public void selectProprietaire() {
+		// recup du parametre id de <f:param>
+		Map<String, String> params = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap();
 		int idProp = Integer.parseInt(params.get("id"));
 		System.out.println("===> MB : PARAM ID : " + idProp);
 
-		//on va chercher le proprio ï¿½ update avec son id
-		Proprietaire proprietaire1 = getProprietaireService().getProprietaireById(idProp);
-		System.out.println("===> MB : PROPRIETAIRE RECUPERERE : "+ proprietaire1);
+		// on va chercher le proprio ï¿½ update avec son id
+		Proprietaire proprietaire1 = getProprietaireService()
+				.getProprietaireById(idProp);
+		System.out.println("===> MB : PROPRIETAIRE RECUPERERE : "
+				+ proprietaire1);
 		setProprietaire(proprietaire1);
 	}
 
 	/**
-	 * Rï¿½cupï¿½ration de l'identifiant du client choisi dans la page etrecherche du client correspondant dans la base de donnï¿½es.
+	 * Rï¿½cupï¿½ration de l'identifiant du client choisi dans la page et
+	 * recherche du client correspondant dans la base de donnï¿½es.
 	 */
 	public void selectClient() {
 		/* Rï¿½cupï¿½ration du paramï¿½tre id de <f:param>. */
@@ -245,7 +261,35 @@ public class AgenceBean implements Serializable {
 						+ client + ". =====");
 		setClient(client);
 	}
-	
+
+	/**
+	 * Récupération de l'identifiant du conseiller choisi dans la page et
+	 * recherche du conseiller correspondant dans la base de données.
+	 */
+	public void selectConseiller() {
+		System.out
+				.println("===== AgenceBean.java - Entrée dans la méthode selectConseiller. =====");
+
+		/* Récupération du paramètre id de <f:param>. */
+		Map<String, String> params = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap();
+		int idConseiller = Integer.parseInt(params.get("id"));
+		System.out
+				.println("===== AgenceBean.java, méthode selectConseiller - idConseiller = "
+						+ idConseiller + ". =====");
+
+		/* Recherche du client à afficher par son identifiant. */
+		Conseiller conseiller = getConseillerService().getConseillerByID(
+				idConseiller);
+		System.out
+				.println("===== AgenceBean.java, méthode selectConseiller - Conseiller récupéré : "
+						+ (conseiller != null) + ". =====");
+		setClient(client);
+
+		System.out
+				.println("===== AgenceBean.java - Sortie de la méthode selectConseiller. =====");
+	}
+
 	/**
 	 * 
 	 */
@@ -255,42 +299,46 @@ public class AgenceBean implements Serializable {
 	}
 
 	public void deleteProprietaire() {
-		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		Map<String, String> params = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap();
 		int idProp = Integer.parseInt(params.get("id"));
 		System.out.println("===> MB DELETE PROPRIO : PARAM ID : " + idProp);
 		getProprietaireService().deleteProprietaire(idProp);
-		//System.out.println("===> dans DELETE get ALL :");
-		//getListeProprietaires();
+		// System.out.println("===> dans DELETE get ALL :");
+		// getListeProprietaires();
 	}
-	
+
 	/**
 	 * Login session
+	 * 
 	 * @return
 	 */
 	public String login() {
 		// RÃ©cupÃ©ration du nom d'utilisateur
-		//		UIParameter cp = (UIParameter) event.getComponent().findComponent("loginPage_username");
-		//		String username = (String) cp.getValue();
+		// UIParameter cp = (UIParameter)
+		// event.getComponent().findComponent("loginPage_username");
+		// String username = (String) cp.getValue();
 
 		// RÃ©cupÃ©ration du mot de passe
-		//		cp = (UIParameter) event.getComponent().findComponent("loginPage_password");
-		//		String password = (String) cp.getValue();
+		// cp = (UIParameter)
+		// event.getComponent().findComponent("loginPage_password");
+		// String password = (String) cp.getValue();
 
 		String username = conseiller.getLogin();
 		String password = conseiller.getPassword();
 		System.out.println("===> MB : login : " + username + " ; " + password);
 
-		Conseiller conseillerTmp = conseillerService.getConseillerByLogin(username, password);
+		Conseiller conseillerTmp = conseillerService.getConseillerByLogin(
+				username, password);
 
-		if(conseillerTmp!=null){
+		if (conseillerTmp != null) {
 			conseiller = utilisateur = conseillerTmp;
-			return("validationIdentificationConseiller");
-		}else{
+			return ("validationIdentificationConseiller");
+		} else {
 			FacesContext.getCurrentInstance().addMessage(
 					null,
 					new FacesMessage(FacesMessage.SEVERITY_WARN,
-							"Invalid Login!",
-							"Please Try Again!"));
+							"Invalid Login!", "Please Try Again!"));
 			return "identificationConseiller.xhtml";
 		}
 	}
@@ -299,8 +347,10 @@ public class AgenceBean implements Serializable {
 	 * Logout session
 	 */
 	public String logout() {
-		System.out.println("===> MB : deconnexion de " + utilisateur.getLogin());
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		System.out
+				.println("===> MB : deconnexion de " + utilisateur.getLogin());
+		FacesContext.getCurrentInstance().getExternalContext()
+				.invalidateSession();
 		return "deconnexion";
 	}
 
@@ -310,51 +360,61 @@ public class AgenceBean implements Serializable {
 	 */
 	public List<Conseiller> getListeConseillers() {
 		System.out.println("===> MB : getting listeConseillers");
-		System.out.println("===> MB : managed property conseillerService : " + conseillerService);
+		System.out.println("===> MB : managed property conseillerService : "
+				+ conseillerService);
 		listeConseillers = getConseillerService().getAllConseillers();
 		System.out.println("===> MB : liste recuperee : " + listeConseillers);
 		return listeConseillers;
 	}
+
 	/**
 	 * 
 	 * @return la liste des Proprietaire
 	 */
 	public List<Proprietaire> getListeProprietaires() {
 		System.out.println("===> MB : getting listeProprietaires");
-		System.out.println("===> MB : managed property proprietaireService : " + proprietaireService);
+		System.out.println("===> MB : managed property proprietaireService : "
+				+ proprietaireService);
 		listeProprietaires = getProprietaireService().getAllProprietaires();
 		System.out.println("===> MB : liste recuperee : " + listeProprietaires);
 		return listeProprietaires;
 	}
+
 	/**
 	 * Rï¿½cupï¿½ration de la liste de tous les clients.
+	 * 
 	 * @return
 	 */
 	public List<Client> getListeClients() {
 		System.out.println("===> MB : getting listeClients");
-		System.out.println("===> MB : managed property clientService : " + clientService);
+		System.out.println("===> MB : managed property clientService : "
+				+ clientService);
 		listeClients = getClientService().getAllClients();
 		System.out.println("===> MB : liste recuperee : " + listeClients);
 		return listeClients;
 	}
+
 	/**
 	 * 
 	 * @return la liste de tous les biens
 	 */
 	public List<BienALouer> getListeBiensALouer() {
 		System.out.println("===> MB : getting listeBiensALouer");
-		System.out.println("===> MB : managed property bienService : " + bienService);
+		System.out.println("===> MB : managed property bienService : "
+				+ bienService);
 		listeBiensALouer = getBienService().getAllBiensALouer();
 		System.out.println("===> MB : liste recuperee : " + listeBiensALouer);
 		return listeBiensALouer;
 	}
+
 	/**
 	 * 
 	 * @return la liste de tous les biens
 	 */
 	public List<BienAAcheter> getListeBiensAAcheter() {
 		System.out.println("===> MB : getting listeBiensAAcheter");
-		System.out.println("===> MB : managed property bienService : " + bienService);
+		System.out.println("===> MB : managed property bienService : "
+				+ bienService);
 		listeBiensAAcheter = getBienService().getAllBiensAAcheter();
 		System.out.println("===> MB : liste recuperee : " + listeBiensAAcheter);
 		return listeBiensAAcheter;
@@ -366,21 +426,26 @@ public class AgenceBean implements Serializable {
 	 */
 	public List<ClasseStandard> getListeClasseStandard() {
 		System.out.println("===> MB : getting listeClasseStandard");
-		System.out.println("===> MB : managed property ClasseStandardService : " + classeStandardService);
+		System.out
+				.println("===> MB : managed property ClasseStandardService : "
+						+ classeStandardService);
 		listeClasseStandard = getClasseStandardService().getAllClasseStandard();
-		System.out.println("===> MB : liste recuperee : " + listeClasseStandard);
+		System.out
+				.println("===> MB : liste recuperee : " + listeClasseStandard);
 		return listeClasseStandard;
 	}
 
-	/* 
+	/*
 	 * autres getters et setters
-	 **/
+	 */
 	public void setListeConseillers(List<Conseiller> listeConseillers) {
 		this.listeConseillers = listeConseillers;
 	}
+
 	public Conseiller getConseiller() {
 		return conseiller;
 	}
+
 	public void setConseiller(Conseiller conseiller) {
 		this.conseiller = conseiller;
 	}
@@ -388,9 +453,11 @@ public class AgenceBean implements Serializable {
 	public void setListeProprietaires(List<Proprietaire> listeProprietaires) {
 		this.listeProprietaires = listeProprietaires;
 	}
+
 	public Proprietaire getProprietaire() {
 		return proprietaire;
 	}
+
 	public void setProprietaire(Proprietaire proprietaire) {
 		this.proprietaire = proprietaire;
 	}
@@ -398,6 +465,7 @@ public class AgenceBean implements Serializable {
 	public Conseiller getUtilisateur() {
 		return utilisateur;
 	}
+
 	public void setUtilisateur(Conseiller utilisateur) {
 		this.utilisateur = utilisateur;
 	}
@@ -405,9 +473,11 @@ public class AgenceBean implements Serializable {
 	public Client getClient() {
 		return client;
 	}
+
 	public void setClient(Client client) {
 		this.client = client;
 	}
+
 	public void setListeClients(List<Client> listeClients) {
 		this.listeClients = listeClients;
 	}
@@ -415,9 +485,11 @@ public class AgenceBean implements Serializable {
 	public BienALouer getBienALouer() {
 		return bienALouer;
 	}
+
 	public void setBienALouer(BienALouer bienALouer) {
 		this.bienALouer = bienALouer;
 	}
+
 	public void setListeBiensALouer(List<BienALouer> listeBiensALouer) {
 		this.listeBiensALouer = listeBiensALouer;
 	}
@@ -425,9 +497,11 @@ public class AgenceBean implements Serializable {
 	public BienAAcheter getBienAAcheter() {
 		return bienAAcheter;
 	}
+
 	public void setBienAAcheter(BienAAcheter bienAAcheter) {
 		this.bienAAcheter = bienAAcheter;
 	}
+
 	public void setListeBiensAAcheter(List<BienAAcheter> listeBiensAAcheter) {
 		this.listeBiensAAcheter = listeBiensAAcheter;
 	}
@@ -444,22 +518,23 @@ public class AgenceBean implements Serializable {
 		this.classeStandard = classeStandard;
 	}
 
-
-
-	/* 
+	/*
 	 * getter and SETTER of the managed properties
 	 */
 	public IConseillerService getConseillerService() {
 		return conseillerService;
 	}
+
 	public void setConseillerService(IConseillerService conseillerService) {
-		System.out.println("===> MB : setting conseiller service to " + conseillerService);
+		System.out.println("===> MB : setting conseiller service to "
+				+ conseillerService);
 		this.conseillerService = conseillerService;
 	}
 
 	public IProprietaireService getProprietaireService() {
 		return proprietaireService;
 	}
+
 	public void setProprietaireService(IProprietaireService proprietaireService) {
 		this.proprietaireService = proprietaireService;
 	}
@@ -467,6 +542,7 @@ public class AgenceBean implements Serializable {
 	public IClientService getClientService() {
 		return clientService;
 	}
+
 	public void setClientService(IClientService clientService) {
 		this.clientService = clientService;
 	}
@@ -474,6 +550,7 @@ public class AgenceBean implements Serializable {
 	public IBienService getBienService() {
 		return bienService;
 	}
+
 	public void setBienService(IBienService bienService) {
 		this.bienService = bienService;
 	}
@@ -492,7 +569,5 @@ public class AgenceBean implements Serializable {
 			return "oui";
 		return "non";
 	}
-
-
 
 }
